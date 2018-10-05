210f8ce10717cdb8530d450775e52fdfbeba63c2
William Kulha <kulha.william@gmail.com>
2018-10-05 14:11:46 -0400

    Add spec so that if a word begins with a consonant, the consonant is sliced and added + 'ay' to the end of the word.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@
 function isVowel(string) {
   return (/^[aeiou]$/i).test(string);
 }
-
+function isConsonant(string) {
+  return (/^[qwrtpsdfghjklzxcvbnm]$/i).test(string);
+}
 function pigLatinize(string) {
   if (isVowel(string[0]) === true) {
     return string + 'way';
   } else if (string[0] === 'y' || string[0] === 'Y') {
     return string.slice(1) + 'yay';
+  } else if (isConsonant(string[0]) === true) {
+    let firstLetter = string[0];
+    return string.slice(1) + firstLetter + 'ay';
   } else {
     return string;
   }
